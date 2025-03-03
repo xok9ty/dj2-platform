9625cb773074bf8853ca2013f9a29ac104f1fc1d
Кирилл <125658205+xok9ty@users.noreply.github.com>
2025-03-03 11:48:41 +0200

    create models class: Subject, Teacher, Class, Student

--- school_project/models.py
+++ school_project/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=100)
+    bio = models.TextField()
+    num_classs = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.title
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    birthday = models.DateField()
+
+    def __str__(self):
+        return self.title
+    
+class Class(models.Model):
+    num_class = models.CharField(max_length=10)
+    floor = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.title
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    birthday = models.DateField()
+    class_num = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.title
