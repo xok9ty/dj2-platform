638cf5b8a77ed7a42971584b22dc9d8b9b8da718
Кирилл <125658205+xok9ty@users.noreply.github.com>
2025-03-03 12:09:29 +0200

    add ForeignKey

--- school_project/models.py
+++ school_project/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Subject(models.Model):
-    subject_name = models.CharField(max_length=100)
-    bio = models.TextField()
-    num_classs = models.IntegerField()
-
-    def __str__(self):
-        return self.title
-
 class Teacher(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -19,12 +11,16 @@ class Teacher(models.Model):
     def __str__(self):
         return self.title
     
-class Class(models.Model):
-    num_class = models.IntegerField()
-    floor = models.IntegerField()
+
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=100)
+    bio = models.TextField()
+    num_classs = models.IntegerField()
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.title
+
 
 class Student(models.Model):
     first_name = models.CharField(max_length=50)
@@ -35,6 +31,15 @@ class Student(models.Model):
     home_adress = models.CharField(max_length=200)
     head_teacher = models.CharField(max_length=100)
     class_num = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+    
+
+class Class(models.Model):
+    num_class = models.IntegerField()
+    floor = models.IntegerField()
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
